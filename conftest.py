7039e9ecca325611389ca7663ceaf03ab4b5fe85
mickBoat00 <m.boateng0000@gmail.com>
2022-08-19 13:28:07 +0000

    tested models in course app

--- conftest.py
+++ conftest.py
@@ -1,7 +1,33 @@
 import pytest
+from pytest_factoryboy import register
+
+from tests.factories import CourseFactory, CurriculumFactory, LessonFactory, YearFactory
+
+register(CurriculumFactory)
+register(YearFactory)
+register(CourseFactory)
+register(LessonFactory)
 
 
-@pytest.fixture(scope="session")
-def test_fixture1():
-    print("Run each test")
-    return 1
+@pytest.fixture
+def course_curriculum(db, curriculum_factory):
+    curriculum = curriculum_factory.create()
+    return curriculum
+
+
+@pytest.fixture
+def course_year(db, year_factory):
+    year = year_factory.create()
+    return year
+
+
+@pytest.fixture
+def course(db, course_factory):
+    course = course_factory.create()
+    return course
+
+
+@pytest.fixture
+def course_lesson(db, lesson_factory):
+    lesson = lesson_factory.create()
+    return lesson
